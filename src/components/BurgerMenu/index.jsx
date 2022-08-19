8818e10406034e39cdac67a2f4b3602886f648b8
Stark <serj.elessar@gmail.com>
2022-08-19 21:31:28 +0300

    remove hyml layout

--- src/components/BurgerMenu/index.jsx
+++ src/components/BurgerMenu/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { VscClose } from 'react-icons/vsc'
-import {AiOutlineMail} from 'react-icons/ai'
 
 import { setCloseMenu } from '../../redux/actions/burgerMenu'
 import MenuList from './components/MenuList'
 import logoImg from '@img/logo.png'
+import ContactInfo from '../ContactInfo'
 
 const BurgerMenu = () => {
   const dispatch = useDispatch()
@@ -25,29 +25,7 @@ const BurgerMenu = () => {
           alt="QPlast Logo"
         />
         <MenuList items={menuItems} />
-        <div className="burger-menu__info">
-          <a 
-            className='burger-menu__info-link' 
-            href="tel:+375293529362"
-          >
-            <span className='burger-menu__info-icon'>A1</span>
-            <span className='burger-menu__info-text'>+375 29 352 93 62</span>
-          </a>
-          <a 
-            className='burger-menu__info-link' 
-            href="tel:+375295750465"
-          >
-            <span className='burger-menu__info-icon'>MTS</span>
-            <span className='burger-menu__info-text'>+375 29 575 04 65</span>
-          </a>
-          <a 
-            className='burger-menu__info-link' 
-            href="mailto:qplast@tut.by"
-          >
-            <span className='burger-menu__info-icon'><AiOutlineMail /></span>
-            <span className='burger-menu__info-text'>qplast@tut.by</span>
-          </a>
-        </div>
+        <ContactInfo isBurger />
       </div>
     </div>
   )

badd8d193cb472c7b6e048c955aea6d9d1d4a410
Stark <serj.elessar@gmail.com>
2022-08-19 19:45:28 +0300

    fix: add info

--- src/components/BurgerMenu/index.jsx
+++ src/components/BurgerMenu/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { VscClose } from 'react-icons/vsc'
+import {AiOutlineMail} from 'react-icons/ai'
+
 import { setCloseMenu } from '../../redux/actions/burgerMenu'
 import MenuList from './components/MenuList'
-import logoImg from '../../assets/images/logo.png'
+import logoImg from '@img/logo.png'
 
 const BurgerMenu = () => {
   const dispatch = useDispatch()
@@ -23,6 +25,29 @@ const BurgerMenu = () => {
           alt="QPlast Logo"
         />
         <MenuList items={menuItems} />
+        <div className="burger-menu__info">
+          <a 
+            className='burger-menu__info-link' 
+            href="tel:+375293529362"
+          >
+            <span className='burger-menu__info-icon'>A1</span>
+            <span className='burger-menu__info-text'>+375 29 352 93 62</span>
+          </a>
+          <a 
+            className='burger-menu__info-link' 
+            href="tel:+375295750465"
+          >
+            <span className='burger-menu__info-icon'>MTS</span>
+            <span className='burger-menu__info-text'>+375 29 575 04 65</span>
+          </a>
+          <a 
+            className='burger-menu__info-link' 
+            href="mailto:qplast@tut.by"
+          >
+            <span className='burger-menu__info-icon'><AiOutlineMail /></span>
+            <span className='burger-menu__info-text'>qplast@tut.by</span>
+          </a>
+        </div>
       </div>
     </div>
   )

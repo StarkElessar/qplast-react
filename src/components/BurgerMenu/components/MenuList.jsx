7457ca403df3264464ef3b2df3d84187192c652e
Stark <serj.elessar@gmail.com>
2022-08-19 12:29:12 +0300

    fix

--- src/components/BurgerMenu/components/MenuList.jsx
+++ src/components/BurgerMenu/components/MenuList.jsx
@@ -1,19 +1,44 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { IoMdArrowDropright } from 'react-icons/io'
 
-const MenuList = ({ items }) => (
-  <ul className='burger-menu__list'>
-    {
-      items.map(({ id, label, path, subList }) => (
-        <li className='burger-menu__item' key={id}>
-          <NavLink to={path} className='burger-menu__link'>
-            {label}
-          </NavLink>
-          {subList && <MenuList items={subList} />}
-        </li>
-      ))
-    }
-  </ul>
-);
+import { setCloseMenu } from '../../../redux/actions/burgerMenu'
+
+const MenuList = ({ items }) => {
+  const dispatch = useDispatch()
+  const [isVisible, setIsVisible] = useState(false)
+
+  const onCloseBurgerMenu = () => dispatch(setCloseMenu())
+  const onShowSubList = () => setIsVisible(!isVisible)
+
+  return (
+    <ul className='burger-menu__list'>
+      {
+        items.map(({ id, label, path, subList, icon }) => (
+          <li className='burger-menu__item' key={id}>
+            <div className={`burger-menu__block ${isVisible ? 'active' : ''}`}>
+              <NavLink
+                to={path}
+                className='burger-menu__link'
+                onClick={onCloseBurgerMenu}
+              >
+                {label}
+              </NavLink>
+              {icon &&
+                <span
+                  className='burger-menu__icon'
+                  onClick={onShowSubList}
+                >
+                  <IoMdArrowDropright />
+                </span>}
+            </div>
+            {subList && <MenuList items={subList} />}
+          </li>
+        ))
+      }
+    </ul>
+  )
+}
 
 export default MenuList;

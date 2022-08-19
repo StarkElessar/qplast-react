a61a1d0983a2817a7c912351a32604e4e2726cf7
Stark <serj.elessar@gmail.com>
2022-08-19 19:46:12 +0300

    add style module

---
+++ src/components/HeaderTop/components/Item/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import s from './index.module.scss'
+
+const Item = ({ icon, text, path, link }) => {
+
+  return (
+    <div className={s.wrapper}>
+      <span className={s.icon}>{icon}</span>
+      {
+        path
+          ? (<Link to={path} className={s.link}>{text}</Link>)
+          : link
+            ?
+            <a href={link} className={s.link}>{text}</a>
+            :
+            <span className={s.text}>{text}</span>
+      }
+    </div>
+  )
+}
+
+export default Item

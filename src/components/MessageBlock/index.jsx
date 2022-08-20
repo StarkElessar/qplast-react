a0ce32df9d3903c9df2a0c88801a165202467177
Stark <serj.elessar@gmail.com>
2022-08-20 16:54:20 +0300

    create new component

---
+++ src/components/MessageBlock/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import s from './index.module.scss'
+
+const MessageBlock = ({ BackIcon, ListIcon, textList, children }) => {
+  return (
+    <div className={s.messageBlock}>
+      <BackIcon className={s.icon} />
+      <ul className={s.list}>
+        {
+          textList.map((obj) => (
+            <li className={s.item} key={obj.id}>
+              <ListIcon />
+              <span>{obj.label}</span>
+            </li>
+          ))
+        }
+        {children}
+      </ul>
+    </div>
+  )
+}
+
+export default MessageBlock

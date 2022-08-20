0fed8b3783dee18c0877a7b691313c3162f552e5
Stark <serj.elessar@gmail.com>
2022-08-20 16:52:45 +0300

    import new reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 
 import burgerMenuReducer from './burgerMenuReducer'
 import footerReducer from './footerReducer'
+import textDataReducer from './textDataReducer'
 
 const rootReducer = combineReducers({
   burgerMenu: burgerMenuReducer,
-  footer: footerReducer
+  footer: footerReducer,
+  textData: textDataReducer
 })
 
 export default rootReducer

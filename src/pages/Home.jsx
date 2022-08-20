e9d802393da1221be2879b4684089467b838045c
Stark <serj.elessar@gmail.com>
2022-08-20 16:53:30 +0300

    delete tag div

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import SectionPromo from '../components/SectionPromo'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <SectionPromo/>
+    </>
   )
 }
 

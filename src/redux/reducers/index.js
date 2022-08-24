80da8324e0a847aec2c2c162537e2c7a68dafb3c
Stark <serj.elessar@gmail.com>
2022-08-24 09:39:03 +0300

    import newsReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux'
 
 import burgerMenuReducer from './burgerMenuReducer'
 import footerReducer from './footerReducer'
+import newsReducer from './newsReducer'
 import textDataReducer from './textDataReducer'
 
 const rootReducer = combineReducers({
   burgerMenu: burgerMenuReducer,
   footer: footerReducer,
-  textData: textDataReducer
+  textData: textDataReducer,
+  news: newsReducer
 })
 
 export default rootReducer

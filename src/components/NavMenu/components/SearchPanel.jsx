c6514da269483d3a07e3e0bd4277dba633c0f78b
Stark <serj.elessar@gmail.com>
2022-09-03 19:32:15 +0300

    fix: search panel

--- src/components/NavMenu/components/SearchPanel.jsx
+++ src/components/NavMenu/components/SearchPanel.jsx
@@ -1,35 +1,16 @@
-import React, { memo, useEffect, useState } from 'react'
+import { memo, useState } from 'react'
 import { AiOutlineSearch } from 'react-icons/ai'
 
 const SearchPanel = () => {
   const [isVisible, setIsVisible] = useState(false)
-  const [minWindow, setMinWindow] = useState(false)
 
-  const onVisibleSearch = () => {
-    if (minWindow) {
-      setIsVisible(!isVisible)
-    } else {
-      return
-    }
-  }
-
-  const checkWidthWindow = () => window.innerWidth <= 1024 ? setMinWindow(true) : setMinWindow(false)
-    
-  useEffect(() => {
-    window.addEventListener('resize', checkWidthWindow)
-
-    return () => {
-      window.addEventListener('resize', checkWidthWindow)
-    }
-  }, [minWindow])
-
-
+  const onVisibleSearch = () => setIsVisible(!isVisible)
 
   return (
-    <div className="nav-menu__search">
+    <div className='nav-menu__search'>
       <input
         className={`nav-menu__search-input ${isVisible ? 'active' : ''}`}
-        type="text"
+        type='text'
         placeholder='Поиск по сайту'
       />
       <AiOutlineSearch

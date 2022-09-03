b02d25aa8a385b5fa5043e2b06de45ab0cd7d099
Stark <serj.elessar@gmail.com>
2022-09-03 19:28:55 +0300

    fix: rename, add scroll to top component..

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import ScrollToTop from '@src/utils/ScrollToTop'
 import React, { memo } from 'react'
 import { useSelector } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { Transition } from 'react-transition-group'
 
 import AppRouter from './components/AppRouter'
 import BurgerMenu from './components/BurgerMenu'
@@ -10,8 +12,12 @@ const App = () => {
 
   return (
     <BrowserRouter>
-      <AppRouter />
-      {isVisibleBurgerMenu && <BurgerMenu />}
+      <ScrollToTop>
+        <AppRouter />
+        <Transition in={isVisibleBurgerMenu} timeout={350}>
+          {(stateClasses) => <BurgerMenu classTransition={stateClasses} />}
+        </Transition>
+      </ScrollToTop>
     </BrowserRouter>
   )
 }

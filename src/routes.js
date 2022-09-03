341ef3ecee601b49adc6509f4ba14d32ecb3931d
Stark <serj.elessar@gmail.com>
2022-09-03 19:29:11 +0300

    add: new routes

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,22 @@ import Shipping from './pages/Shipping'
 import News from './pages/News'
 import About from './pages/About'
 import Contacts from './pages/Contacts'
+import Soglashenie from './pages/Soglashenie'
+import PrivacyPolicy from './pages/PrivacyPolicy'
+import Vakansii from './pages/Vakansii'
+import OnePost from './components/OnePost'
 
 import {
   ABOUT_ROUTE,
+  ARTICLES_ROUTE,
   CATALOG_ROUTE,
   CONTACTS_ROUTE,
   HOME_ROUTE,
   NEWS_ROUTE,
-  SHIPPING_PAYMENT_ROUTE
+  PRIVACY_POLICY_ROUTE,
+  SHIPPING_PAYMENT_ROUTE,
+  SOGLASHENIE_ROUTE,
+  VAKANSII_ROUTE
 } from './utils/constsPath'
 
 export const publicRoutes = [
@@ -19,6 +27,11 @@ export const publicRoutes = [
   { path: CATALOG_ROUTE, Component: Catalog },
   { path: SHIPPING_PAYMENT_ROUTE, Component: Shipping },
   { path: NEWS_ROUTE, Component: News },
+  { path: ARTICLES_ROUTE, Component: News },
+  { path: `${NEWS_ROUTE}/:id`, Component: OnePost },
   { path: ABOUT_ROUTE, Component: About },
   { path: CONTACTS_ROUTE, Component: Contacts },
+  { path: SOGLASHENIE_ROUTE, Component: Soglashenie },
+  { path: PRIVACY_POLICY_ROUTE, Component: PrivacyPolicy },
+  { path: VAKANSII_ROUTE, Component: Vakansii },
 ]

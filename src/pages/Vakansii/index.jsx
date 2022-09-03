570a3f17430bbb0afaf3b547f0320bf5daa5c002
Stark <serj.elessar@gmail.com>
2022-09-03 19:34:42 +0300

    feature: new page and styles

---
+++ src/pages/Vakansii/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import s from './index.module.scss'
+
+const Vakansii = () => {
+  return (
+    <section className='vakansii__container'>Vakansii</section>
+  )
+}
+
+export default Vakansii

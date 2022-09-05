0ff367a11b27864192b1ac8ba08c00882425a3d9
Stark <serj.elessar@gmail.com>
2022-09-05 12:48:06 +0300

    fix: add vacancy component

--- src/pages/Vakansii/index.jsx
+++ src/pages/Vakansii/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import Vacancy from '@components/Vacancy'
 
 import s from './index.module.scss'
 
 const Vakansii = () => {
+  const { allVacancy } = useSelector(({ textData }) => textData)
+
   return (
-    <section className='vakansii__container'>Vakansii</section>
+    <section className='vakansii__container'>
+      <h1 className={s.titleH1}>Вакансии</h1>
+      {allVacancy.map((vacancy) => (
+        <Vacancy key={vacancy.id} items={vacancy} />
+      ))}
+    </section>
   )
 }
 

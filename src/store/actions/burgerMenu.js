f859db154a109e08ac42ed3d384759705369329f
Stark <serj.elessar@gmail.com>
2022-09-07 15:09:07 +0300

    fix: changed import path

--- src/store/actions/burgerMenu.js
+++ src/store/actions/burgerMenu.js
@@ -1,4 +1,4 @@
-import { SET_VISIBLE_MENU, SET_CLOSE_MENU } from "../../utils/constsActionTypes";
+import { SET_VISIBLE_MENU, SET_CLOSE_MENU } from "@src/utils/constsActionTypes";
 
 export const setVisibleMenu = () => ({ type: SET_VISIBLE_MENU })
 export const setCloseMenu = () => ({ type: SET_CLOSE_MENU })
